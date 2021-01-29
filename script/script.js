0999e89cf3923e937844e16d28e24d295078cc47
Evgeniy Filatov <zhenechka.filatov.86@mail.ru>
2021-01-29 12:06:39 +0300

    исправление

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 let money = 40000;
 let income = 'Фриланс';
-let addExpenses = 'Проезд на такси, Бензин на авто,Коммунальные расходы';
+let addExpenses = 'Проезд на такси,Бензин на авто,Коммунальные расходы';
 let deposit = true;
-let mission = 30000;
+let mission =  ' цель 30000 рублей';
 let period = '6 месяцев';
+let budGetDay = 1000
+budGetDay *=  30;
 
-
+console.log(money);
+console.log(income);
+console.log(deposit);
+console.log(addExpenses.length);
+console.log(period + mission);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split(','));
+console.log('доход за месяц', budGetDay)
