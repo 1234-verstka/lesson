0e19add86ac5e958876f3fddc1ece437514a07ee
Evgeniy Filatov <zhenechka.filatov.86@mail.ru>
2021-01-29 10:30:49 +0300

    третий день

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 40000;
+let income = 'Фриланс';
+let addExpenses = 'Проезд на такси, Бензин на авто,Коммунальные расходы';
+let deposit = true;
+let mission = 30000;
+let period = '6 месяцев';
 
-alert('Hello Maks');
-console.log('Привет куратор');
+
