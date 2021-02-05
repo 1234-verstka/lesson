3fa39d803a00ba05ba3dcdc68af3020c427b8661
Evgeniy Filatov <zhenechka.filatov.86@mail.ru>
2021-02-05 10:37:44 +0300

    Пятый урок

--- script/script.js
+++ script/script.js
@@ -11,22 +11,21 @@ const isNumber = (n) => {
 let money,
   income = 'фриланс',
   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
-      'интернет, такси, коммуналка'),
+      'садик, такси, коммуналка'),
   deposit = confirm('Есть ли у вас депозит в банке?'),
   mission = 100000,
   period = 12;
 
-// 1) Переписать функцию start циклом do while
+
 do {
   money = prompt('Ваш месячный доход?');
 } while (!isNumber(money));
 
 let expenses = [];
-// 2) Добавить проверку что введённые данные являются числом, которые мы получаем 
-// на вопрос 'Во сколько это обойдется?’ в функции  getExpensesMonth
+
 const getExpensesMonth = () => {
   let sum = 0;
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 2; i++) {
       expenses[i] = prompt('Введите обязательную статью расходов?');
       sum += (() => {
           let n = 0;
@@ -69,8 +68,7 @@ showTypeOf(deposit);
 console.log(addExpenses.toLocaleLowerCase().split(', '));
 console.log('Обязательные расходы за месяц: ', expensesAmount);
 
-// 3) Если getTargetMonth возвращает нам отрицательное значение, то вместо “Цель будет достигнута” 
-// необходимо выводить “Цель не будет достигнута”
+
 (targetMonth >= 0) ?
   console.log(`Цель будет достигнута за: ${targetMonth} месяцев`) :
   console.log(`Цель не будет достигнута`);
